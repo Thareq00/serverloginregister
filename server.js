8b059cc8a43ef77efd237dc642a951d6a1bde648
Thareq Aqsha <thareq.aqsha@gmail.com>
2026-05-12 13:59:50 +0700

    clean node_modules

--- server.js
+++ server.js
@@ -1,44 +1,126 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
 app.use(express.json());
 
-/* HUBUNGKAN FRONTEND */
+/* FRONTEND */
 app.use(express.static(path.join(__dirname, "public")));
 
+/* FILE DATABASE */
+const USERS_FILE = "users.json";
+
+/* AMBIL USER */
+function getUsers() {
+
+    if (!fs.existsSync(USERS_FILE)) {
+        fs.writeFileSync(USERS_FILE, "[]");
+    }
+
+    return JSON.parse(
+        fs.readFileSync(USERS_FILE)
+    );
+}
+
+/* SIMPAN USER */
+function saveUsers(users) {
+
+    fs.writeFileSync(
+        USERS_FILE,
+        JSON.stringify(users, null, 2)
+    );
+
+}
+
+/* HOME */
 app.get("/", (req, res) => {
+
     res.sendFile(
         path.join(__dirname, "public", "index.html")
     );
-});
-/* LOGIN */
-app.post("/login", (req, res) => {
-
-    const { username, password } = req.body;
-
-    console.log(username, password);
-
-    res.json({
-        message: "Login berhasil",
-        token: "123456"
-    });
 
 });
 
 /* REGISTER */
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
 
     const { username, password } = req.body;
 
-    console.log(username, password);
+    const users = getUsers();
+
+    /* CEK USER */
+    const userExists = users.find(
+        user => user.username === username
+    );
+
+    if (userExists) {
+
+        return res.json({
+            message: "Username sudah digunakan"
+        });
+
+    }
+
+    /* HASH PASSWORD */
+    const hashedPassword =
+        await bcrypt.hash(password, 10);
+
+    /* SIMPAN USER */
+    users.push({
+        username,
+        password: hashedPassword
+    });
+
+    saveUsers(users);
 
     res.json({
         message: "Register berhasil"
     });
 
 });
+
+/* LOGIN */
+app.post("/login", async (req, res) => {
+
+    const { username, password } = req.body;
+
+    const users = getUsers();
+
+    const user = users.find(
+        user => user.username === username
+    );
+
+    if (!user) {
+
+        return res.json({
+            message: "User tidak ditemukan"
+        });
+
+    }
+
+    const validPassword =
+        await bcrypt.compare(
+            password,
+            user.password
+        );
+
+    if (!validPassword) {
+
+        return res.json({
+            message: "Password salah"
+        });
+
+    }
+
+    res.json({
+        message: "Login berhasil"
+    });
+
+});
+
 app.listen(3000, () => {
     console.log("Server berjalan di port 3000");
 });
